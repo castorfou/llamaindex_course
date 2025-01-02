be2bb57f2d7a73d376b899b32416d4f74dbd65f6
guillaume <guillaume.ramelet@michelin.com>
2025-01-02 16:38:40 +0100

    helper pour llm et embed_model

--- nbs/helper.py
+++ nbs/helper.py
@@ -2,15 +2,21 @@
 
 import os
 from dotenv import load_dotenv, find_dotenv
+from llama_index.llms.azure_openai import AzureOpenAI
+from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
+from llama_index.core import Settings
 
-# these expect to find a .env file at the directory above the lesson.                                                                                                                     # the format for that file is (without the comment)                                                                                                                                       #API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService                                                                                                                                     
+
+# these expect to find a .env file at the directory above the lesson.                                                                                                                     # the format for that file is (without the comment)                                                                                                                                       #API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService
 def load_env():
     _ = load_dotenv(find_dotenv())
+
 
 def get_openai_api_key():
     load_env()
     openai_api_key = os.getenv("OPENAI_API_KEY")
     return openai_api_key
+
 
 def get_azure_openai_keys():
     load_env()
@@ -18,3 +24,27 @@ def get_azure_openai_keys():
     azure_endpoint = os.getenv("AZURE_ENDPOINT")
     azure_api_version = os.getenv("AZURE_API_VERSION")
     return azure_api_key, azure_endpoint, azure_api_version
+
+
+def get_azure_llm(engine="gpt-4o"):
+    AZURE_API_KEY, AZURE_ENDPOINT, AZURE_API_VERSION = get_azure_openai_keys()
+    llm = AzureOpenAI(
+        engine=engine,
+        api_key=AZURE_API_KEY,
+        azure_endpoint=AZURE_ENDPOINT,
+        api_version=AZURE_API_VERSION,
+    )
+    Settings.llm = llm
+    return llm
+
+
+def get_azure_embed_model(model="text-embedding-ada-002"):
+    AZURE_API_KEY, AZURE_ENDPOINT, AZURE_API_VERSION = get_azure_openai_keys()
+    embed_model = AzureOpenAIEmbedding(
+        model=model,
+        api_key=AZURE_API_KEY,
+        azure_endpoint=AZURE_ENDPOINT,
+        api_version=AZURE_API_VERSION,
+    )
+    Settings.embed_model = embed_model
+    return embed_model
--- nbs/utils.py
+++ nbs/utils.py
@@ -8,78 +8,59 @@ from llama_index.core.tools import QueryEngineTool
 from llama_index.core.query_engine.router_query_engine import RouterQueryEngine
 from llama_index.core.selectors import LLMSingleSelector
 from llama_index.llms.azure_openai import AzureOpenAI
-from llama_index.embeddings.azure_openai  import AzureOpenAIEmbedding
+from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
 
-from helper import get_azure_openai_keys
+from helper import get_azure_openai_keys, get_azure_llm, get_azure_embed_model
 from llama_index.core import Settings
 
 
-
-def get_router_query_engine(file_path: str, llm = None, embed_model = None):
+def get_router_query_engine(file_path: str, llm=None, embed_model=None):
     """Get router query engine."""
     llm = llm or OpenAI(model="gpt-3.5-turbo")
     embed_model = embed_model or OpenAIEmbedding(model="text-embedding-ada-002")
     Settings.llm = llm
     Settings.embed_model = embed_model
-    
+
     # load documents
     documents = SimpleDirectoryReader(input_files=[file_path]).load_data()
-    
+
     splitter = SentenceSplitter(chunk_size=1024)
     nodes = splitter.get_nodes_from_documents(documents)
-    
+
     summary_index = SummaryIndex(nodes)
     vector_index = VectorStoreIndex(nodes)
-    
+
     summary_query_engine = summary_index.as_query_engine(
         response_mode="tree_summarize",
         use_async=True,
     )
     vector_query_engine = vector_index.as_query_engine()
-    
+
     summary_tool = QueryEngineTool.from_defaults(
         query_engine=summary_query_engine,
-        description=(
-            "Useful for summarization questions related to MetaGPT"
-        ),
+        description=("Useful for summarization questions related to MetaGPT"),
     )
-    
+
     vector_tool = QueryEngineTool.from_defaults(
         query_engine=vector_query_engine,
-        description=(
-            "Useful for retrieving specific context from the MetaGPT paper."
-        ),
+        description=("Useful for retrieving specific context from the MetaGPT paper."),
     )
-    
+
     query_engine = RouterQueryEngine(
         selector=LLMSingleSelector.from_defaults(),
         query_engine_tools=[
             summary_tool,
             vector_tool,
         ],
-        verbose=True
+        verbose=True,
     )
     return query_engine
 
 
 def get_router_query_engine_from_azure(file_path: str):
 
+    llm = get_azure_llm()
 
-    AZURE_API_KEY, AZURE_ENDPOINT, AZURE_API_VERSION = get_azure_openai_keys()
-
-    llm = AzureOpenAI(
-        engine="gpt-4o",
-        api_key=AZURE_API_KEY,
-        azure_endpoint=AZURE_ENDPOINT,
-        api_version=AZURE_API_VERSION,
-    )
-
-    # You need to deploy your own embedding model as well as your own chat completion model
-    embed_model = AzureOpenAIEmbedding(
-        model="text-embedding-ada-002",
-        api_key=AZURE_API_KEY,
-        azure_endpoint=AZURE_ENDPOINT,
-        api_version=AZURE_API_VERSION,
-    )
+    embed_model = get_azure_embed_model()
 
     return get_router_query_engine(file_path, llm=llm, embed_model=embed_model)
